db5b79c16381d866a9115268777a370e2dc2715a
Enrique Valverde Soriano <43791767+Kirkuss@users.noreply.github.com>
2019-01-23 21:44:41 +0100

    Server nuevo

--- Server.py
+++ Server.py
@@ -1,75 +1,71 @@
 #!/usr/bin/python3
-# -*- mode:python; coding:utf-8; tab-width:4 -*-
+# -*- mode:python; conding:utf-8; tab-width:4 -*-
 
 import binascii
 import Ice
 Ice.loadSlice('downloader.ice')
 # pylint: disable=E0401
 import Downloader
-import work_queue
-from queue import Queue
+import sys
 
-class TransferI(Downloader.Transfer):
-    '''
-    Transfer file
-    '''
-    def __init__(self, local_filename):
-        self.file_contents = open(local_filename, 'rb')
+from work_queue import *
 
-    def recv(self, size, current=None):
-        '''Send data block to client'''
-        return str(
-            binascii.b2a_base64(self.file_contents.read(size), newline=False)
-        ) #devuelve una representacion imprimible de un objeto, en base64
-
-    def end(self, current=None):
-        '''Close transfer and free objects'''
-        self.file_contents.close()
-        current.adapter.remove(current.id)
-
-#TODO:
-#Revisar la implementacion de DownloadScheduler porque faltan muchas cosas
 
 class DownloadSchedulerI(Downloader.DownloadScheduler):
+	def __init__(self, work_queue):
+		self.work_queue = work_queue	
 
-	def __init__(self):
-		self.wq = work_queue.WorkQueue()
-		self.songList = []
-	
-	def getSongList(self):
-		return self.songList
+	def addDownloadTask_async(self, cb, url, current=None):
+		self.work_queue.add(cb, url)
 
-	def addDownloadTask(self, URL):
-		self.songList.append(URL)
-		self.wq.add(callback, URL)
+	def cancelTask(self, url, current=None):
+		print("{0} recibido".format(url))
+		sys.stdout.flush()
 
-	def get(self, song): #Ni idea de que pretende esta funcion
-		##
+class TransferI(Downloader.Transfer):
+	def __init__(self, local_filename):
+		self.file_contents = open(local_filename, 'rb')
 
-	def cancelTask(URL): #Esto tiene que estar mal pero mas o menos tiene que ser asi como funcione
-		AuxJobs = self.wq.queue
-		for Job in AuxJobs:
-			j = AuxJobs.get()
-			if j.url == URL:
-				j.cancel()
-			else:
-				print("The job doesn't exists, no Jobs where removed from the queue")
-			
-		
+	def recv(self, size, current=None):
+		return str(binascii.b2a_base64(self.file_contents.read(size), newline=False))
 
-#TODO:
-#Implementar la funcionalidad principal del servidor 'run()'
-#Probar el funcionamiento sin los canales de comunicacion y una vez este funcione implementar el uso de dichos canales y configurar el grid
-
-'''
+	def end(self, current=None):
+		self.file_contents.close()
+		current.adapter.remove(current.id)
 
 class Server(Ice.Application):
-	
-	#def get topic manager
-	
 	def run(self, args):
-		servant = TransferI()
+		work_queue = WorkQueue()
+		broker = self.communicator()
+		servant = DownloadSchedulerI(work_queue)
+		
+		adapter = broker.createObjectAdapter("SchedulerAdapter")
+		proxy = adapter.add(servant, broker.stringToIdentity("Scheduler1"))
+		
+		print(proxy, flush=True)
+	
+		adapter.activate()
+		print("adapter activated")
 
-'''
+		work_queue.start()
+
+		self.shutdownOnInterrupt()
+		broker.waitForShutdown()
+
+		work_queue.destroy()
+
+		return 0
+
+if __name__ == '__main__':
+	app = Server()
+	exit_status = app.main(sys.argv)
+	sys.exit(exit_status)
+
+
+
+	
+
+
 	
 	
+		
