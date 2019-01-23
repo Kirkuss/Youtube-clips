3ecfce75066981ac39714909ea7dbcc6bc2a1ca2
Enrique Valverde Soriano <43791767+Kirkuss@users.noreply.github.com>
2019-01-23 21:44:20 +0100

    Cliente nuevo

--- Client.py
+++ Client.py
@@ -3,48 +3,80 @@
 
 import binascii
 import sys
-import ice
+import time
+import Ice
 Ice.loadSlice('downloader.ice')
 # pylint: disable=E0401
 import Downloader
 
 BLOCK_SIZE = 10240
 
-#TODO:
-#Implementar la interfaz ProgressEvent (entender como funciona struct en python).
+class DownloaderCB(object):
+	def response(self, retval):
+		print("Callback: Value: {0}".format(retval))
+	
+	def failure(self, ex):
+		print("Ex: {0}".format(ex))
 
-class ProgressEventI(Downloader.ProgressEvent):	
-
-	def notify(clipData):
-		
-
-def receive(transfer, destination_file):
-
-    with open(destination_file, 'wb') as file_contents:
-        remoteEOF = False
-        while not remoteEOF:
-            data = transfer.recv(BLOCK_SIZE)
-            if len(data) > 1:
-                data = data[1:]
-            data = binascii.a2b_base64(data)
-			remoteEOF = len(data) < BLOCK_SIZE
-			if data:
-				file_contents.write(data)
-		transfer.end()
 
 class Client(Ice.Application):
-
-	def run(self, argv):
-		proxy = self.communicator().stringToProxy(argv[1])
-		transfer = Downloader.transferPrx.checkedCast(proxy)
-
-		if not transfer:
-			raise RunTimeError('Invalid proxy')
-
-		transfer.addDownloadTask(argv[2])
+	def sendURL(self, scheduler, cb):		
+		url = "http://www.youtube.com/watch?v=LDU_Txk06tM"
+		"""url = input("URL to download: ")"""	
+		print("enviando")
+		scheduler.begin_addDownloadTask(url, cb.response, cb.failure)
+		print("donete")
 
 		return 0
 
+	def run(self, argv):
+		proxy = self.communicator().stringToProxy(argv[1])
+		scheduler = Downloader.DownloadSchedulerPrx.checkedCast(proxy)
+
+		if not scheduler:
+			raise RuntimeError('Invalid proxy')
+
+		downloader_cb = DownloaderCB()
+
+		option = 1
+		
+		while True:	
+			print("--MENU--")
+			print("1-Ask for download")
+			print("2-Info about download process")
+			print("3-Obtain audio files")
+			print("4-Obtain list of downloaded files")
+			print("Press any number to exit")
+			option = int(input("Choose an option from the menu: "))
+			if option == 1:
+				self.sendURL(scheduler, downloader_cb)
+			elif option == 2:
+				continue
+			elif option == 3:
+				"""receive()"""
+				continue
+			elif option == 4:
+				continue
+			else:
+				break
+	
+		return 0
+
+
+	def receive(self, transfer, destination_file): #transfer es un objeto Downloader.Transfer
+		with open(destination_file, 'wb') as file_contents:
+			remoteEOF = False
+			while not remoteEOF:
+				data = transfer.recv(BLOCK_SIZE)
+				if len(data) > 1:
+					data = data[1:]
+				data = binascii.a2b_base64(data)
+				remoteEOF = len(data) < BLOCK_SIZE
+				if data:
+					file_contents.write(data)
+			transfer.end()
+
 sys.exit(Client().main(sys.argv))
+		
 	
 	
